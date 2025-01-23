2f56bb275583e7f8d9853471fa08f293e30a36fc
xyl_t9 <dbsrjs1224@gmail.com>
2025-01-23 23:57:27 +0900

    [2025-01-23] Movement

--- Assets/0. Scripts/Board.cs
+++ Assets/0. Scripts/Board.cs
@@ -3,10 +3,20 @@ using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
-    public Tilemap tilemap {  get; private set; }
-    public Piece activePiece { get; private set; } 
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
-    public Vector3Int spawnPosition;
+    public Vector3Int spawnPosition;        //스폰 위치
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -34,10 +44,50 @@ public class Board : MonoBehaviour
 
     public void Set(Piece piece)
     {
-        for(int i  = 0; i < piece.cells.Length; i++)
+        for (int i = 0; i < piece.cells.Length; i++)
         {
             Vector3Int tilePosition = piece.cells[i] + piece.position;
             tilemap.SetTile(tilePosition, piece.data.tile);
         }
+    }
+    
+    /// <summary>
+    /// 이동하기 전 잔상 제거
+    /// </summary>
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    /// <summary>
+    /// 유효한 위치인지 체크
+    /// </summary>
+    /// <returns></returns>
+    public bool IsValidPosition(Piece piece, Vector3Int position)
+    {
+        RectInt bounds = Bounds;
+
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            //맵을 벗어나지 않았는지 판단
+            if(!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            //해당 공간을 차지하고 있는지 판단
+            if(tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Assets/0. Scripts/Piece.cs
+++ Assets/0. Scripts/Piece.cs
@@ -13,7 +13,7 @@ public class Piece : MonoBehaviour
         this.position = position;
         this.data = data;
 
-        if(cells == null)
+        if (cells == null)
         {
             cells = new Vector3Int[data.cells.Length];
         }
@@ -22,5 +22,61 @@ public class Piece : MonoBehaviour
         {
             cells[i] = (Vector3Int)data.cells[i];
         }
+    }
+
+    private void Update()
+    {
+        board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+
+        board.Set(this);
+    }
+
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
+
+    /// <summary>
+    /// 좌, 우, 하 이동
+    /// </summary>
+    /// <param name="translation">이동할 방향</param>
+    /// <returns>이동 할 수 있는지 없는지</returns>
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = board.IsValidPosition(this, newPosition);
+
+        //이동할 수 있는 자리인지 확인 후 이동
+        if (valid)
+        {
+            position = newPosition;
+        }
+
+        return valid;
     }
 }
